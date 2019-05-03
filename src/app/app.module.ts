ccd03dc1fef763fb7074485078865ab9ad59fe31
Edmond Fox <fox.edmond.g@gmail.com>
2019-05-03 14:50:40 +0300

    Add form fields and setup two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import {
   MatBadgeModule,
   MatButtonModule,
@@ -26,6 +27,8 @@ import { HomepageComponent } from './homepage/homepage.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    FormsModule,
 
     MatBadgeModule,
     MatButtonModule,
