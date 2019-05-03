6646030cbcb8fce0feee07f8e450771c6cf5be73
Edmond Fox <fox.edmond.g@gmail.com>
2019-05-03 14:04:41 +0300

    Create character class, add property to Characters Component, show character with interpolation binding

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../character';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.css']
+  styleUrls: [ './characters.component.css' ]
 })
 export class CharactersComponent implements OnInit {
+  character: Character = {
+    id: 1,
+    name: 'Tyrion',
+    status: 'alive'
+  };
 
   constructor() { }
 
